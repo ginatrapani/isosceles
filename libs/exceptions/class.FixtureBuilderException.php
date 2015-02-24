5688292941368af43e0b3a26c34ec393bf3df9c0
Gina Trapani <ginatrapani@gmail.com>
2015-02-24 14:26:52 -0500

    Remove duplicate fixture library

---
+++ libs/exceptions/class.FixtureBuilderException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * LICENSE:
+ *
+ * This file is part of Isosceles (http://ginatrapani.github.io/isosceles/).
+ *
+ * Isosceles is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Isosceles is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with Isosceles.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @license http://www.gnu.org/licenses/gpl.html
+ */
+
+class FixtureBuilderException extends Exception {}
