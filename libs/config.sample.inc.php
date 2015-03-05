548b64e039a12ebcd5b8ea7dc5ed6c5d5ea83965
Gina Trapani <ginatrapani@gmail.com>
2015-03-05 12:31:17 -0500

    Switch to require for test setUp

--- libs/config.sample.inc.php
+++ libs/config.sample.inc.php
@@ -98,5 +98,5 @@ if (isset($_SESSION["RD_MODE"]) || getenv("RD_MODE")=="1") {
 
 $ISOSCELES_CFG['custom_app_config']             = dirname(dirname(__FILE__)).'/www-example/config.inc.php';
 if (isset($ISOSCELES_CFG['custom_app_config']) && file_exists($ISOSCELES_CFG['custom_app_config'] )) {
-    require_once($ISOSCELES_CFG['custom_app_config']);
+    require($ISOSCELES_CFG['custom_app_config']);
 }
